709f0a998e67efa7dbf9a6a0aacd00d0f5d3f0ca
Rafael-co <rafael.camilo21@outlook.com>
2022-09-19 17:58:24 -0300

    operadores logicos e comparadores

---
+++ 19.09.22/operadores_de_conversão.js
@@ -0,0 +1,28 @@
+const msg = 'eu sou uma mensagem';
+const msg2 = 'eu sou outra mensagem';
+
+console.log(msg == msg2);//false
+
+const num =5;
+const num2= 5;
+
+console.log(num==num2);//true
+
+let a = 78;
+let b ='78';
+
+console.log(a==b);//true igual(valor, ginorando o tipo do conteudo)
+
+console.log(a===b);//fALSE identico( valor e tipo tem quer iguais)
+
+console.log(msg != msg2)// True diferen de :
+
+console.log(a != b)//false este diferente nao leva em consideracao o tipo
+
+console.log(a !== b)//true pq aqui ele compara e leva em consideracao a tipagem 
+
+console.log(a<b)
+
+console.log(a>b);
+
+console.log(a>=b);
---
+++ 19.09.22/operadores_logicos.js
@@ -0,0 +1,24 @@
+const media =(3.5+6+10)/3
+const presenca =0.7
+
+console.log(media >= 7 && presenca >=0.8); //operador E
+
+//operador ou
+console.log(4 > 7|| 3 < 2)
+
+//operador de negação
+//tabela verdade
+// not V -> F
+//not F -> V
+//ele inverte o valor booleando resultante
+
+const resultado = 5<3 ;
+
+console.log(!resultado);
+
+
+const logado = true
+
+if(!logado){
+    console.log('n pode')
+}
