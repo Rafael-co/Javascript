279a29d87a225f5a79adec2f160b520716bc5695
Rafael-co <rafael.camilo21@outlook.com>
2022-09-27 17:31:32 -0300

    correção da atividade

--- atividade27.09.js
+++ atividade27.09.js
@@ -1,25 +1,27 @@
 /**
  *Crie um algortimo que recebe informações de 15 produtos. Cada produto possui nome, marca, uma foto (link de uma imagem da internet), preço unitário e a quantidade de produtos em estoque. Ao receber essas informações dos produtos, imprima no HTML essas informações de cada produto e o produto entre o preço e a quantidade de produtos em estoque (preço total).
  */
+let produtos = []
 
  let produto ={
-    nome:[],
-    marca:[],
-    foto:[],
-    precoUnitario:[],
-    estoque:[],
+    nome:'',
+    marca:'',
+    foto:'',
+    precoUnitario:0,
+    estoque:0,
 
  }
 
 
- for (i=0;i < 15;i++){
-    produto.nome.push(prompt(`Digite o nome do ${i+1}º produto`));
-    produto.marca.push(prompt(`Digite a marca do(a) ${produto.nome[i]} `))
-    produto.foto.push(prompt(`Insira o link da imagem do(a) ${produto.nome[i]}`))
-    produto.precoUnitario.push(parseFloat(prompt(`Digite o preço do(a) ${produto.nome[i]}`)))
-    produto.estoque.push(parseFloat(prompt(`Digite a quantidade no estoque do(a) ${produto.nome[i]}`)))
+ for (i = 0;i < 4;i++){
+    produto.nome=(prompt(`Digite o nome do ${i+1}º produto`));
+    produto.marca=(prompt(`Digite a marca do(a) ${produto.nome} `))
+    produto.foto=(prompt(`Insira o link da imagem do(a) ${produto.nome}`))
+    produto.precoUnitario=(parseFloat(prompt(`Digite o preço do(a) ${produto.nome}`)))
+    produto.estoque=(parseInt(prompt(`Digite a quantidade no estoque do(a) ${produto.nome}`)))
+    produtos.push(produto)
 
-    document.write(`nome do produto : ${produto.nome[i]} <br> marca: ${produto.marca[i]} <br> foto: <img src="${produto.foto[i]}"> <br> preço : ${produto.precoUnitario[i]} <br> quantidade no estoque: ${produto.estoque[i]}<br> ` )
+    document.write(`nome do produto : ${produtos[i].nome} <br> marca: ${produtos[i].marca} <br> foto: <img src="${produtos[i].foto}"> <br> preço :R$ ${produtos[i].precoUnitario} <br> quantidade no estoque: ${produtos[i].estoque}<br> ` )
 
 
  }
